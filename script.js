b78aedeb5752b57f996f5d99e3e5c1327294e2bc
Xpexdex <73250542+Xpexdex@users.noreply.github.com>
2021-05-15 15:17:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 //In progress
 var response;
 var data;
-while (true) {
-response = await fetch("https://api.scratch.mit.edu/users/mybearworld/messages/count/");
-data = await response.json();
-document.getElementById("messages").innerHTML = data;
-}
+
+var requestPlayerData = function(user,pass,passIn,item,asignTo){
+  if (passIn) {
+    response = await fetch("https://api.scratch.mit.edu/users/" + user /messages/count/);
+                         } else {
+                            response = await fetch("https://api.scratch.mit.edu/users/" + user + item);
+                           }
+  data = await response.json();
+  document.getElementById(asignTo).innerHTML = data;
+};
+requestPlayerData(Xpexdex,blank,false,"/messages/count/");
 
