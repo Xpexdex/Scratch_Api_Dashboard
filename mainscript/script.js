efe2e1e06882f5540de0595aa5188bccffc209b6
Xpexdex <73250542+Xpexdex@users.noreply.github.com>
2021-05-15 14:46:18 -0400

    Update script.js

--- mainscript/script.js
+++ mainscript/script.js
@@ -1,2 +1,9 @@
 //In progress
+var response;
 var data;
+while (true) {
+response = await fetch("https://api.scratch.mit.edu/users/mybearworld/messages/count/");
+data = await response.json();
+document.getElementById("")
+}
+
